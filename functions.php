c2cf47720ded1c00fdc1e2608b5b1f4acc4bb71d
Miina Sikk <miina.sikk@gmail.com>
2018-03-27 19:26:37 +0300

    Add index.php file placeholder. Add copied style from Travel theme.

--- functions.php
+++ functions.php
@@ -12,6 +12,23 @@ function travel_enqueue_editor_scripts()
 		get_template_directory_uri() . '/assets/js/editor-blocks.js',
 		array( 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' )
 	);
+
+	// This will be needed for featured block for the sample image URLs.
+	wp_localize_script(
+		'travel-editor-blocks-js',
+		'travel_globals',
+		array(
+			'theme_url' => esc_url( get_template_directory_uri() ),
+		)
+	);
+
+	// This file's content is directly copied from the Travel theme static template.
+	// @todo Use only style that's actually needed within the editor.
+	wp_enqueue_style(
+		'travel-editor-blocks-css',
+		get_template_directory_uri() . '/blocks/editor-blocks.css',
+		array( 'wp-blocks' )
+	);
 }
 
 // Hook into block editor assets.
