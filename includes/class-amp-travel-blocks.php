376284c39a9a43d1d00f8ee2f3452a4f08840afd
Ryan Kienstra <kienstraryan@gmail.com>
2018-04-12 12:31:30 -0500

    Indent 2 lines, whitespace only.
    This was the only remaining issue in the PR.
    So I made this change on my own to merge it.

--- includes/class-amp-travel-blocks.php
+++ includes/class-amp-travel-blocks.php
@@ -113,8 +113,8 @@ class AMP_Travel_Blocks {
 			$output .= '</div>
 							</div>
 							<span class="travel-results-result-subtext mr1">' .
-				/* translators: %d: The number of reviews */
-				sprintf( esc_html__( '%d Reviews', 'travel' ), esc_html( $comments->approved ) ) . '</span>
+								/* translators: %d: The number of reviews */
+								sprintf( esc_html__( '%d Reviews', 'travel' ), esc_html( $comments->approved ) ) . '</span>
 							<span class="travel-results-result-subtext"><svg class="travel-icon" viewBox="0 0 77 100"><g fill="none" fillRule="evenodd"><path stroke="currentColor" strokeWidth="7.5" d="M38.794 93.248C58.264 67.825 68 49.692 68 38.848 68 22.365 54.57 9 38 9S8 22.364 8 38.85c0 10.842 9.735 28.975 29.206 54.398a1 1 0 0 0 1.588 0z"></path><circle cx="38" cy="39" r="10" fill="currentColor"></circle></g></svg>
 							' . esc_html( $location ) . '</span>
 						</div>
