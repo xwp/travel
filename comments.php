9d60672f8b72a8a03756dc89f8b6ca024df0b043
Miina Sikk <miina.sikk@gmail.com>
2018-04-16 19:13:20 +0300

    Start adding theme implementation for single-adventure.php

---
+++ comments.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Comments template.
+ *
+ *  @package WPAMPTheme
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+
+?>
+
+<div class="product-reviews">
+
+	<h2 class="mb3"><?php esc_html_e( 'Reviews', 'travel' ); ?></h2>
+
+	<div class="comments wrap__item">
+
+		<amp-live-list id="amp-live-comments-list-<?php the_ID(); ?>" class="live-list" layout="container" data-poll-interval="<?php echo esc_attr( AMP_TRAVEL_LIVE_LIST_POLL_INTERVAL ); ?>" data-max-items-per-page="<?php echo esc_attr( get_option( 'page_comments' ) ? get_option( 'comments_per_page' ) : 10000 ); ?>">
+			<ol items class="comments__list">
+				<?php if ( empty( $comments ) ): ?>
+					<?php esc_html_e( 'No comments found', 'travel' ); ?>
+				<?php else: ?>
+					<li data-sort-time="1519880447" data-update-time="1519880447" id="comment-32" class="comment byuser comment-author-ryan-kienstra even thread-even depth-1">
+						<article id="div-comment-32" class="comment-body"><footer class="comment-meta"><div class="comment-author vcard">
+									<amp-img alt="" src="https://secure.gravatar.com/avatar/ea4feefa34607a9eda9d5478dac38a1e?s=32&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/ea4feefa34607a9eda9d5478dac38a1e?s=64&amp;d=mm&amp;r=g 2x" class="avatar avatar-32 photo amp-wp-enforced-sizes" height="32" width="32" layout="intrinsic"></amp-img><strong class="fn">Ryan Kienstra</strong>
+									<div class="inline-block relative ml1 h3 line-height-2">
+										<div class="travel-results-result-stars green">★★★★★</div>
+									</div>
+								</div>
+								<!-- .comment-author -->
+
+								<div class="comment-metadata">
+									<time datetime="2018-03-01T05:00:47+00:00">March 1, 2018 at 5:00 am</time>
+								</div>
+								<!-- .comment-metadata -->
+
+							</footer><!-- .comment-meta --><div class="comment-content">
+								<p>This is a test comment.</p>
+							</div>
+							<!-- .comment-content -->
+
+							<a rel="nofollow" class="comment-reply-link" href="#respond" on='tap:AMP.setState( {"commentform_post_106":{"replyToName":"Ryan Kienstra","values":{"comment_parent":"32"}}} )' aria-label="Reply to Ryan Kienstra">Reply</a>			</article><!-- .comment-body -->
+					</li>
+					<!-- #comment-## -->
+				<?php endif; ?>
+			</ol>
+			<!-- .comment-list --><div update class="live-list__button">
+				<button class="button" on="tap:amp-live-comments-list-106.update">New comment(s)</button>
+			</div>
+			<nav pagination></nav></amp-live-list><div id="respond" class="comment-respond">
+			<h2 id="reply-title" class="comment-reply-title">
+				<span [text]='commentform_post_106.replyToName ? "Leave a Reply to " + commentform_post_106.replyToName + "" : "Leave a Reply"'>Leave a Reply</span> <small><a id="cancel-comment-reply-link" href="/2018/02/06/elon-musks-new-tesla-pay-package-is-a-publicity-stunt-thats-also-real/#respond" hidden [hidden]='commentform_post_106.values.comment_parent == "0"' on='tap:AMP.setState( {"commentform_post_106":{"replyToName":"","values":{"comment_parent":"0"}}} )'>Cancel reply</a></small>
+			</h2>			<form method="post" id="commentform" class="comment-form" novalidate action-xhr="https://dev-ampconfdemo.pantheonsite.io/wp-comments-post.php?_wp_amp_action_xhr_converted=1" target="_top" on='submit:AMP.setState( { "commentform_post_106": { submitting: true } } );submit-error:AMP.setState( { "commentform_post_106": { submitting: false } } );submit-success:AMP.setState( { commentform_post_106: {"values":{"submit":"Post Comment","comment_post_ID":"106","comment_parent":"0","comment":""},"submitting":false,"replyToName":""} } )'>
+				<amp-state id="commentform_post_106"><script type="application/json">{"values":{"author":"","email":"","url":"","submit":"Post Comment","comment_post_ID":"106","comment_parent":"0","comment":""},"submitting":false,"replyToName":""}</script></amp-state>
+				<p class="comment-form-comment"><label for="comment">Comment</label> <textarea class="rounded" id="comment" name="comment" cols="45" rows="8" maxlength="65525" required="required" [disabled]="commentform_post_106.submitting" [text]="commentform_post_106.values.comment" on='change:AMP.setState( { commentform_post_106: { values: { "comment": event.value } } } )'></textarea></p>
+				<p class="comment-form-author"><label for="author">Name <span class="required">*</span></label> <input class="rounded" id="author" name="author" type="text" value="" size="30" maxlength="245" required="required" [disabled]="commentform_post_106.submitting" [value]="commentform_post_106.values.author" on='change:AMP.setState( { commentform_post_106: { values: { "author": event.value } } } )'></p>
+				<p class="comment-form-email"><label for="email">Email <span class="required">*</span></label> <input class="rounded" id="email" name="email" type="email" value="" size="30" maxlength="100" aria-describedby="email-notes" required="required" [disabled]="commentform_post_106.submitting" [value]="commentform_post_106.values.email" on='change:AMP.setState( { commentform_post_106: { values: { "email": event.value } } } )'></p>
+				<p class="comment-form-url"><label for="url">Website</label> <input class="rounded" id="url" name="url" type="url" value="" size="30" maxlength="200" [disabled]="commentform_post_106.submitting" [value]="commentform_post_106.values.url" on='change:AMP.setState( { commentform_post_106: { values: { "url": event.value } } } )'></p>
+				<p class="comment-form-rating"><label for="rating">Your rating</label>
+					<select class="select-arr rounded" name="rating" id="rating" [disabled]="commentform_post_106.submitting" on='change:AMP.setState( { commentform_post_106: { values: { "rating": event.value } } } )'>
+						<option value="1">1 star</option>
+						<option value="2">2 stars</option>
+						<option value="3">3 stars</option>
+						<option value="4">4 stars</option>
+						<option value="5">5 stars</option>
+					</select>
+				</p>
+				<p class="form-submit"><input name="submit" type="submit" id="submit" class="submit ampstart-btn rounded center bold white block col-12" value="Post review" [disabled]="commentform_post_106.submitting" [value]="commentform_post_106.values.submit" on='change:AMP.setState( { commentform_post_106: { values: { "submit": event.value } } } )'><input type="hidden" name="comment_post_ID" value="106" id="comment_post_ID" [disabled]="commentform_post_106.submitting" [value]="commentform_post_106.values.comment_post_ID" on='change:AMP.setState( { commentform_post_106: { values: { "comment_post_ID": event.value } } } )'><input type="hidden" name="comment_parent" id="comment_parent" value="0" [disabled]="commentform_post_106.submitting" [value]="commentform_post_106.values.comment_parent" on='change:AMP.setState( { commentform_post_106: { values: { "comment_parent": event.value } } } )'></p>
+				<div submit-success>
+					<template type="amp-mustache"><p>{{{message}}}</p>
+					</template>
+				</div>
+				<div submit-error>
+					<template type="amp-mustache"><p class="amp-comment-submit-error">{{{error}}}</p>
+					</template>
+				</div>
+			</form>
+		</div>
+		<!-- #respond -->
+
+	</div>
+
+</div>
+<!-- / product-reviews -->
--- includes/functions.php
+++ includes/functions.php
@@ -6,10 +6,137 @@
  */
 
 /**
+ * Live list poll interval.
+ *
+ * @var integer
+ */
+const AMP_TRAVEL_LIVE_LIST_POLL_INTERVAL = 15000;
+
+/**
  * Init theme.
  *
  * @return object Theme object
  */
 function amp_travel_theme() {
 	return AMP_Travel_Theme::get_instance();
+}
+
+/**
+ * Display similar adventures.
+ */
+function amp_travel_render_similar_adventures() {
+	/*$terms = wp_get_post_terms( null, 'location', array(
+		'fields' => 'names',
+	) );*/
+	$adventures = get_posts(
+		array(
+			'post_type'   => 'adventure',
+			'numberposts' => 3,
+			'meta_key'    => '_thumbnail_id',
+			/*'tax_query'   => array(
+				array(
+					'taxonomy' => 'location',
+					'field'    => 'slug',
+					'terms'    => $terms,
+				),
+			),*/
+			'exclude'     => array( get_the_ID() ),
+		)
+	);
+
+	$args = array(
+		'heading' => __( 'Similar Adventures', 'travel' ),
+	);
+
+	echo amp_travel_get_popular_adventures( $adventures, $args );
+}
+
+/**
+ * Get HTML for popular adventures.
+ *
+ * @param array $adventures Adventures.
+ * @param array $attributes Attributes.
+ * @return string HTML.
+ */
+function amp_travel_get_popular_adventures( $adventures, $attributes ) {
+	$output = '';
+
+	if ( AMP_Travel_Blocks::$popular_posts_count !== count( $adventures ) ) {
+		return $output;
+	}
+
+	$output .= '<section class="travel-popular pb4 pt3 relative">';
+
+	if ( ! empty( $attributes['heading'] ) ) {
+		$output .= '<header class="max-width-3 mx-auto px1 md-px2">
+				<h3 class="h1 bold line-height-2 md-hide lg-hide" aria-hidden="true">' . esc_html( $attributes['heading'] ) . '</h3>
+				<h3 class="h1 bold line-height-2 xs-hide sm-hide center">' . esc_html( $attributes['heading'] ) . '</h3>
+			</header>';
+	}
+
+	$output .= '<div class="overflow-scroll">
+				<div class="travel-overflow-container">
+					<div class="flex px1 md-px2 mxn1">';
+
+	$popular_classes = array(
+		'travel-popular-tilt-right',
+		'travel-results-result',
+		'travel-popular-tilt-left',
+	);
+
+	foreach ( $adventures as $index => $adventure ) {
+		$attachment_id = get_post_thumbnail_id( $adventure->ID );
+		$img_src       = wp_get_attachment_image_url( $attachment_id, 'full' );
+		$img_srcset    = wp_get_attachment_image_srcset( $attachment_id );
+		$price         = get_post_meta( $adventure->ID, 'amp_travel_price', true );
+		$rating        = round( (int) get_post_meta( $adventure->ID, 'amp_travel_rating', true ) );
+		$comments      = wp_count_comments( $adventure->ID );
+		$locations     = wp_get_post_terms( $adventure->ID, 'location', array(
+			'fields' => 'names',
+		) );
+
+		if ( is_wp_error( $locations ) || empty( $locations ) ) {
+			$location = '--';
+		} else {
+			$location = $locations[0];
+		}
+
+		$output .= '<div class="m1 mt3 mb2"><div class="' . esc_html( $popular_classes[ $index ] ) . ' mb1">
+								<div class="relative travel-results-result">
+									<a class="travel-results-result-link block relative" href="' . esc_url( get_the_permalink( $adventure->ID ) ) . '">
+										<amp-img class="block rounded" width="346" height="200" noloading="" src="' . esc_url( $img_src ) . '" srcset="' . esc_attr( $img_srcset ) . '"></amp-img>
+									</a>
+								</div>
+							</div>
+							<div class="h2 line-height-2 mb1">
+								<span class="travel-results-result-text">' . esc_html( get_the_title( $adventure->ID ) ) . '</span>
+								<span class="travel-results-result-subtext h3">•</span>
+								<span class="travel-results-result-subtext h3">$&nbsp;</span><span class="black bold">' . esc_html( $price ) . '</span>
+							</div>
+							<div class="h4 line-height-2">
+								<div class="inline-block relative mr1 h3 line-height-2">
+									<div class="travel-results-result-stars green">';
+
+		for ( $i = 0; $i < $rating; $i++ ) {
+			$output .= '★';
+		}
+
+		$output .= '</div>
+							</div>
+							<span class="travel-results-result-subtext mr1">' .
+			/* translators: %d: The number of reviews */
+			sprintf( esc_html__( '%d Reviews', 'travel' ), esc_html( $comments->approved ) ) . '</span>
+							<span class="travel-results-result-subtext"><svg class="travel-icon" viewBox="0 0 77 100"><g fill="none" fillRule="evenodd"><path stroke="currentColor" strokeWidth="7.5" d="M38.794 93.248C58.264 67.825 68 49.692 68 38.848 68 22.365 54.57 9 38 9S8 22.364 8 38.85c0 10.842 9.735 28.975 29.206 54.398a1 1 0 0 0 1.588 0z"></path><circle cx="38" cy="39" r="10" fill="currentColor"></circle></g></svg>
+							' . esc_html( $location ) . '</span>
+						</div>
+						</div>';
+
+	}
+
+	$output .= '</div>
+				</div>
+			</div>
+		</section>';
+
+	return $output;
 }
