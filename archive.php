6f5c8701d984ba3d82ce4343366a6798688affd9
Miina Sikk <miina.sikk@gmail.com>
2018-04-25 11:06:54 +0300

    Fixes from review.

--- archive.php
+++ archive.php
@@ -16,7 +16,8 @@ get_header();
 			<div class="travel-inline-list travel-results-list">
 				<div class="max-width-3 mx-auto px1 md-px2">
 					<h1><?php echo wp_kses_post( amp_travel_get_archive_title() ); ?></h1>
-					<div><?php printf( '%d adventures', esc_html( amp_travel_get_posts_count() ) ); ?></div>
+					<?php /* translators: %d: The number of adventures */ ?>
+					<div><?php printf( esc_html( _n( '%d adventure', '%d adventures', amp_travel_get_posts_count(), 'travel' ) ), esc_html( amp_travel_get_posts_count() ) ); ?></div>
 					<?php get_template_part( 'templates/live-lists/posts' ); ?>
 				</div>
 			</div>
--- search.php
+++ search.php
@@ -15,7 +15,8 @@ get_header();
 			<div class="travel-inline-list travel-results-list">
 				<div class="max-width-3 mx-auto px1 md-px2">
 					<?php if ( isset( $_GET['s'] ) ) : ?>
-						<h1><?php printf( '%d Results for <strong>%s</strong>', esc_html( amp_travel_get_posts_count() ), esc_html( $_GET['s'] ) ); ?></h1>
+						<?php /* translators: %d: Number of results; %s: Search phrase. */ ?>
+						<h1><?php printf( esc_html( _n( '%d Result for ', '%d Results for ', amp_travel_get_posts_count(), 'travel' ) ) . '<strong>%s</strong>', esc_html( amp_travel_get_posts_count() ), esc_html( $_GET['s'] ) ); ?></h1>
 					<?php endif; ?>
 					<?php get_template_part( 'templates/live-lists/posts' ); ?>
 				</div>
