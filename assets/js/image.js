a64f6e5743d9d6f6261fe088032d01745fbb7e0c
David Cramer <david@digilab.co.za>
2018-05-01 11:31:54 +0200

    Use tabs not spaces in js

--- assets/js/image.js
+++ assets/js/image.js
@@ -4,80 +4,80 @@
  * JS Functions for extended images in adventure post type.
  */
 
-(function() {
-  'use strict';
+( function() {
+	'use strict';
 
-  jQuery( document ).ready( function( $ ) {
+	jQuery( document ).ready( function( $ ) {
 
-    // Uploading files
-    let fileFrame;
-    let template = $( '#travel-images-tmpl' ).html();
-    jQuery( document ).on( 'click', '.travel-image-select', function( event ) {
+		// Uploading files
+		let fileFrame;
+		let template = $( '#travel-images-tmpl' ).html();
+		jQuery( document ).on( 'click', '.travel-image-select', function( event ) {
 
-      event.preventDefault();
+			event.preventDefault();
 
-      let title = $( this ).data( 'title' );
-      let text = $( this ).data( 'text' );
+			let title = $( this ).data( 'title' );
+			let text = $( this ).data( 'text' );
 
-      // If the media frame already exists, reopen it.
-      if ( fileFrame ) {
-        // Open frame
-        fileFrame.open();
-        return;
-      }
+			// If the media frame already exists, reopen it.
+			if ( fileFrame ) {
+				// Open frame
+				fileFrame.open();
+				return;
+			}
 
-      // Create the media frame.
-      fileFrame = wp.media.frames.fileFrame = wp.media( {
-        title: title,
-        button: {
-          text: text,
-        },
-        multiple: 'add',
-      } );
+			// Create the media frame.
+			fileFrame = wp.media.frames.fileFrame = wp.media( {
+				title: title,
+				button: {
+					text: text,
+				},
+				multiple: 'add',
+			} );
 
-      fileFrame.on( 'open', function() {
-        $( '.travel-images-preview' ).each( function() {
-          fileFrame.state().get( 'selection' ).add( new wp.media.attachment( $( this ).data( 'id' ) ) );
-        } );
-        // Set window to browse tab.
-        fileFrame.content.mode('browse');
-      } );
+			fileFrame.on( 'open', function() {
+				$( '.travel-images-preview' ).each( function() {
+					fileFrame.state().get( 'selection' ).add( new wp.media.attachment( $( this ).data( 'id' ) ) );
+				} );
+				// Set window to browse tab.
+				fileFrame.content.mode( 'browse' );
+			} );
 
-      // When an image is selected, run a callback.
-      fileFrame.on( 'select', function() {
+			// When an image is selected, run a callback.
+			fileFrame.on( 'select', function() {
 
-        let images = fileFrame.state().get( 'selection' );
-        let wrapper = $( '#amp-travel-images-wrap' );
-        // Clear selection from metabox to use the new selection order.
-        wrapper.empty();
-        images.each( function( image ) {
-          let attachment = image.toJSON();
+				let images = fileFrame.state().get( 'selection' );
+				let wrapper = $( '#amp-travel-images-wrap' );
+				// Clear selection from metabox to use the new selection order.
+				wrapper.empty();
+				images.each( function( image ) {
+					let attachment = image.toJSON();
 
-          if ( ! $( '#travel-image-' + attachment.id ).length ) {
+					if ( ! $( '#travel-image-' + attachment.id ).length ) {
 
-            let url = attachment.url;
-            let imageTemplate = template;
-            if ( attachment.sizes.medium ) {
-              url = attachment.sizes.medium.url;
-            }
-            imageTemplate = imageTemplate.replace( /\{\{id\}\}/g, attachment.id ).replace( /\{\{url\}\}/g, url );
-            wrapper.append( imageTemplate );
-          }
-        } );
+						let url = attachment.url;
+						let imageTemplate = template;
+						if ( attachment.sizes.medium ) {
+							url = attachment.sizes.medium.url;
+						}
+						imageTemplate = imageTemplate.replace( /\{\{id\}\}/g, attachment.id ).replace( /\{\{url\}\}/g, url );
+						wrapper.append( imageTemplate );
+					}
+				} );
 
-      } );
+			} );
 
-      // Open the modal.
-      fileFrame.open();
-    } );
+			// Open the modal.
+			fileFrame.open();
+		} );
 
-    // Bind, image remove button to remove the image.
-    jQuery( document ).on( 'click', '.travel-image-control-remove', function( event ) {
-      event.preventDefault();
-      let image = $( this ).data( 'target' );
-      $( '#' + image ).remove();
-    } );
+		// Bind, image remove button to remove the image.
+		jQuery( document ).on( 'click', '.travel-image-control-remove', function( event ) {
+			event.preventDefault();
+			let image = $( this ).data( 'target' );
+			$( '#' + image ).remove();
+		} );
 
-  } );
+	} );
 
 })( window );
